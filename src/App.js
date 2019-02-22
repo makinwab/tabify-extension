e6dd38ccfe87235dab2c5076540836b355151be4
Bukky <makinwa37@gmail.com>
2019-02-22 11:10:10 +0100

    improve UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import logo from './contentful-logo-1.png'
+import { Button } from 'semantic-ui-react'
 import './App.css'
 
 class App extends Component {
@@ -8,17 +9,18 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
+
+          <br />
+
+          <h1>
+            <span className='tabify-logo'>Tabify</span> ➕ <span>Contentful</span> ≌ 😍
+          </h1>
+
+          <h4>Keep your browser tabs clean. Save tabs and check them out later.</h4>
+
+          <br />
+
+          <Button content='Save Tab' />
         </header>
       </div>
     )
