31e91e6ad93bcee3447819410f91243d7ecc5307
Bukky <makinwa37@gmail.com>
2019-03-06 12:57:51 +0100

    allow users add emails or switch accounts

--- src/App/App.js
+++ src/App/App.js
@@ -3,13 +3,13 @@ import { Button, Icon, Label, Form } from 'semantic-ui-react'
 import Tabs from '../Tabs/Tabs'
 import SaveTab from '../Tabs/SaveTab'
 import logo from '../contentful-logo-1.png'
-import { user, getEntries, environment as contentfulClient } from '../Helpers/contentful'
+import client, { getEntries, environment as contentfulClient } from '../Helpers/contentful'
 
 import './App.css'
 const localStorage = window.localStorage
 
-const retrieveUser = () => {
-  return user.then(entry => entry.items.filter(value => value.fields.email === localStorage.getItem('user')))
+const retrieveUser = (email = localStorage.user) => {
+  return client.CDAClient.getEntries({content_type: 'user', 'fields.email': email})
 }
 
 class App extends Component {
@@ -27,10 +27,24 @@ class App extends Component {
   }
 
   componentDidMount () {
+    if (document.querySelector('div.all-good')) {
+      document.querySelector('div.all-good').style.display = 'none'
+    }
+    if (document.querySelector('div.menu-links')) {
+      document.querySelector('div.menu-links').style.display = 'none'
+    }
+    if (document.querySelector('.save-tab-btn')) {
+      document.querySelector('.save-tab-btn').style.display = 'none'
+    }
+
     this.updateUserData()
 
     if (localStorage.user) {
-      document.querySelector('.save-tab-btn').disabled = false
+      this.setState({ email: localStorage.user })
+
+      document.querySelector('Form.user-form button').textContent = 'Switch User'
+      document.querySelector('.save-tab-btn').style.display = 'block'
+      document.querySelector('div.menu-links').style.display = 'block'
     }
   }
 
@@ -45,7 +59,11 @@ class App extends Component {
   }
 
   updateUserData () {
-    retrieveUser().then(user => this.setState({ user: user[0].sys.id }))
+    retrieveUser().then(user => {
+      if(user.items.length > 0) {
+        this.setState({ user: user.items[0].sys.id })
+      }
+    })
   }
 
   handlePageChange (ev, page) {
@@ -61,24 +79,50 @@ class App extends Component {
     this.setState({ [name]: value })
   }
 
-  handleEmailForm = (ev) => {
+  handleUserSwitch = (ev) => {
     ev.preventDefault()
     const { email } = this.state
 
-    contentfulClient.then(environment => environment.createEntry('user', {
-      fields: {
-        email: {
-          'en-US': email,
+    if (!this.isValidEmail(email)) {
+      alert('Please enter a valid email')
+      return
+    }
+    // find or create user
+    retrieveUser(email)
+      .then(result => {
+        if (result.items.length === 0) {
+          contentfulClient.then(environment => environment.createEntry('user', {
+            fields: {
+              email: {
+                'en-US': email,
+              }
+            }
+          }))
+          .then(entry => entry.publish())
+          .then(result => {
+            this.setState({user: result.sys.id})
+          })
+          .catch(console.error)
+        } else {
+          this.setState({ user: result.items[0].sys.id })
         }
-      }
-    }))
-    .then(entry => entry.publish())
-    .then(() => { 
-      localStorage.setItem('user', email)
-      document.querySelector('Form.user-form button').disabled = true
-      document.querySelector('Button.save-tab-btn').disabled = false
-    })
-    .catch(e => console.error)
+      })
+      .then(() => {
+        localStorage.setItem('user', email)
+
+        document.querySelector('Form.user-form button').textContent = 'Switch User'
+        document.querySelector('.save-tab-btn').style.display = 'block'
+        document.querySelector('div.menu-links').style.display = 'block'
+        document.querySelector('div.all-good').style.display = 'block'
+        setTimeout(() => document.querySelector('div.all-good').style.display = 'none', 5000)
+      })
+      .catch(console.error)
+  }
+
+  isValidEmail (email) {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(email)
   }
 
   render () {
@@ -89,7 +133,10 @@ class App extends Component {
         {(page === 'App')
           ? <div id='App' className='App'>
             <div className='menu-links'>
-              <Label color='black' as='a' onClick={ev => this.handlePageChange(ev, { current: 'App', next: 'Tabs' })}>
+              <Label className='save-tab-btn' color='black' as='a' onClick={ev => this.handleSave(ev, { current: 'App', next: 'SaveTab' })}>
+                <Icon name='add' />Save Tab
+              </Label>
+              <Label className='my-tabs-btn' as='a' onClick={ev => this.handlePageChange(ev, { current: 'App', next: 'Tabs' })}>
                 <Icon name='linkify' />My Tabs
               </Label>
             </div>
@@ -97,27 +144,26 @@ class App extends Component {
             <div className='app-main'>
               <header className='App-header'>
 
-                <Form onSubmit={this.handleEmailForm} className='user-form'>
-                  <Form.Group widths='equal'>
-                    <Form.Input name='email' value={email} onChange={this.handleInputChange} fluid label='Enter an email to work with' placeholder='Email'/>
-                  </Form.Group>
-
-                  <Button type='submit'>Use Email</Button>
-                </Form>
-
                 <img src={logo} className='App-logo' alt='logo' />
-
-                <br />
-
                 <h1>
                   <span className='tabify-logo'>Tabify</span> ❣️ <span>Contentful</span>
                 </h1>
 
-                <h4>Keep your browser tabs clean. Save tabs and check them out later.</h4>
-
+                <small>Keep your browser tabs clean. Save tabs and check them out later.</small>
                 <br />
+                <br />
+                <Form onSubmit={this.handleUserSwitch} className='user-form'>
+                  <Form.Group widths='equal'>
+                    <Form.Input name='email' value={email} onChange={this.handleInputChange} fluid label='Enter an email to work with' placeholder='Email'/>
+                    
+                    <Label className='my-tabs-btn all-good'>
+                      <Icon name='check' />Success
+                    </Label>
+                  </Form.Group>
 
-                <Button className='save-tab-btn' content='Save Tab' onClick={ev => this.handleSave(ev, { current: 'App', next: 'SaveTab' })} />
+                  <Button color='orange' basic type='submit'>Add Account</Button>
+                </Form>
+                {/* <Button  content='Save Tab' onClick={ev => this.handleSave(ev, { current: 'App', next: 'SaveTab' })} /> */}
               </header>
             </div>
           </div>
